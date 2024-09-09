77274d4900a649e3fe8ca8a2109450656c288c93
kkmanos <koukoularisman@gmail.com>
2024-09-09 12:39:22 +0300

    removed local authentication components

--- wallet-enterprise-configurations/pda1-issuer/src/configuration/authentication/authenticationChain.ts
+++ wallet-enterprise-configurations/pda1-issuer/src/configuration/authentication/authenticationChain.ts
@@ -1,6 +1,5 @@
 import { CONSENT_ENTRYPOINT, VERIFIER_PANEL_ENTRYPOINT } from "../../authorization/constants";
 import { AuthenticationChainBuilder } from "../../authentication/AuthenticationComponent";
-import { LocalAuthenticationComponent } from "./LocalAuthenticationComponent";
 import { VerifierAuthenticationComponent } from "./VerifierAuthenticationComponent";
 import { IssuerSelectionComponent } from "./IssuerSelectionComponent";
 import { AuthenticationMethodSelectionComponent } from "./AuthenticationMethodSelectionComponent";
@@ -10,7 +9,7 @@ import { RecipientValidationComponent } from './RecipientValidationComponent';
 export const authChain = new AuthenticationChainBuilder()
 	.addAuthenticationComponent(new AuthenticationMethodSelectionComponent("auth-method", CONSENT_ENTRYPOINT))
 	.addAuthenticationComponent(new VIDAuthenticationComponent("vid-authentication", CONSENT_ENTRYPOINT))
-	.addAuthenticationComponent(new LocalAuthenticationComponent("1-local", CONSENT_ENTRYPOINT))
+	// .addAuthenticationComponent(new LocalAuthenticationComponent("1-local", CONSENT_ENTRYPOINT))
 	.addAuthenticationComponent(new RecipientValidationComponent("3-recipient-validation", CONSENT_ENTRYPOINT))
 	.addAuthenticationComponent(new IssuerSelectionComponent("2-issuer-selection", CONSENT_ENTRYPOINT))
 	.build();
